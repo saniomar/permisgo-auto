3c2399ffe355533737b0dc632edab33bdbc94a3d
664Parvez <parvezrabbi664@gmail.com>
2026-03-01 15:29:03 +0600

    update

--- src/app/helps/page.jsx
+++ src/app/helps/page.jsx
@@ -21,11 +21,36 @@ const helps = () => {
                 <h2>Latest driving tips</h2>
                 <p>Newly updated resources for students</p>
               </div>
-              <div className="mt-4"></div>
+              <div className="mt-4">
+                <div className="row">
+                  <div className="col-lg-4">
+                    <div></div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div></div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
-        <section></section>
+        <section>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <h3>Do you still have any questions ?</h3>
+                <p>Our friendly team is here to help you pass your driving test!</p>
+                <Link href="/contact-us">Chat with support</Link>
+              </div>
+              <div className="col-lg-6">
+                <Image src="" layout='responsive' alt=''></Image>
+              </div>
+            </div>
+          </div>
+        </section>
     </>
   )
 }
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -43,30 +43,84 @@ const Register = () => {
                                         </ul>
                                     </div>
                                 </div>
-                                <div className='mt-3' id={registerStyle.register_form}>
-                                    <form action="">
-                                        <div className="row">
-                                            <div className="col-lg-6 mt-3">
-                                                <label htmlFor="firstName">First Name</label>
-                                                <input type="text" id='firstName' className='form-control form-control-lg' name='' placeholder='First Name' required />
-                                            </div>
-                                            <div className="col-lg-6 mt-3">
-                                                <label htmlFor="lastName">Last Name</label>
-                                                <input type="text" id='lastName' className='form-control form-control-lg' name='' placeholder='Last Name' required />
-                                            </div>
-                                            <div className="col-lg-12 mt-3">
-                                                <label htmlFor="email">Email</label>
-                                                <input type="email" id='email' className='form-control form-control-lg' name='' placeholder='Email' required />
-                                            </div>
-                                            <div className="col-lg-12 mt-3">
-                                                <label htmlFor="password">Password</label>
-                                                <input type="password" id='password' className='form-control form-control-lg' name='' placeholder='Password' required />
-                                            </div>
-                                            <div className="col-lg-12 mt-4">
-                                                <button className='btn btn-lg'>Register</button>
-                                            </div>
+                                <div className='mt-3 register_form' id={registerStyle.register_form}>
+
+                                    <ul className="nav nav-tabs gap-3" id="myTab" role="tablist">
+                                        <li className="nav-item" role="presentation">
+                                            <button className="nav-link active" id="student-tab" data-bs-toggle="tab" data-bs-target="#student-tab-pane" type="button" role="tab" aria-controls="student-tab-pane" aria-selected="true">Student Register</button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button className="nav-link" id="teacher-tab" data-bs-toggle="tab" data-bs-target="#teacher-tab-pane" type="button" role="tab" aria-controls="teacher-tab-pane" aria-selected="false">Teacher Register</button>
+                                        </li>
+                                    </ul>
+                                    <div className="tab-content" id="myTabContent">
+                                        <div className="tab-pane fade show active" id="student-tab-pane" role="tabpanel" aria-labelledby="student-tab" tabindex="0">
+                                            <form action="">
+                                                <div className="row">
+                                                    <div className="col-lg-12 mt-3">
+                                                        <div className="form-check">
+                                                            <input type="checkbox" name="" className='form-check-input' value="" id="flexCheckChecked" checked />
+                                                            <label className="form-check-label" for="flexCheckChecked">
+                                                                Student Register
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-lg-6 mt-3">
+                                                        <label htmlFor="firstName">First Name</label>
+                                                        <input type="text" id='firstName' className='form-control form-control-lg' name='' placeholder='First Name' required />
+                                                    </div>
+                                                    <div className="col-lg-6 mt-3">
+                                                        <label htmlFor="lastName">Last Name</label>
+                                                        <input type="text" id='lastName' className='form-control form-control-lg' name='' placeholder='Last Name' required />
+                                                    </div>
+                                                    <div className="col-lg-12 mt-3">
+                                                        <label htmlFor="email">Email</label>
+                                                        <input type="email" id='email' className='form-control form-control-lg' name='' placeholder='Email' required />
+                                                    </div>
+                                                    <div className="col-lg-12 mt-3">
+                                                        <label htmlFor="password">Password</label>
+                                                        <input type="password" id='password' className='form-control form-control-lg' name='' placeholder='Password' required />
+                                                    </div>
+                                                    <div className="col-lg-12 mt-4">
+                                                        <button className='btn btn-lg'>Register</button>
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
-                                    </form>
+                                        <div className="tab-pane fade" id="teacher-tab-pane" role="tabpanel" aria-labelledby="teacher-tab" tabindex="0">
+                                            <form action="">
+                                                <div className="row">
+                                                    <div className="col-lg-12 mt-3">
+                                                        <div className="form-check">
+                                                            <input type="checkbox" name="" className='form-check-input' value="" id="flexCheckChecked" checked />
+                                                            <label className="form-check-label" for="flexCheckChecked">
+                                                                Teacher Register
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-lg-6 mt-3">
+                                                        <label htmlFor="firstName">First Name</label>
+                                                        <input type="text" id='firstName' className='form-control form-control-lg' name='' placeholder='First Name' required />
+                                                    </div>
+                                                    <div className="col-lg-6 mt-3">
+                                                        <label htmlFor="lastName">Last Name</label>
+                                                        <input type="text" id='lastName' className='form-control form-control-lg' name='' placeholder='Last Name' required />
+                                                    </div>
+                                                    <div className="col-lg-12 mt-3">
+                                                        <label htmlFor="email">Email</label>
+                                                        <input type="email" id='email' className='form-control form-control-lg' name='' placeholder='Email' required />
+                                                    </div>
+                                                    <div className="col-lg-12 mt-3">
+                                                        <label htmlFor="password">Password</label>
+                                                        <input type="password" id='password' className='form-control form-control-lg' name='' placeholder='Password' required />
+                                                    </div>
+                                                    <div className="col-lg-12 mt-4">
+                                                        <button className='btn btn-lg'>Register</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
