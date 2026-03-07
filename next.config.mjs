601212a2f7c32a7aa0d38b8d8a96f13357b10d9a
664Parvez <parvezrabbi664@gmail.com>
2026-03-07 14:18:44 +0600

    update1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  exportTrailingSlash: true,
   /* config options here */
   reactCompiler: true,
   devIndicators: true,
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
