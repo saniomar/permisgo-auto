3e64acc6a67b4db3909a77ac0db5ee227cfe0e1b
664Parvez <parvezrabbi664@gmail.com>
2026-03-09 09:42:33 +0600

    update

--- src/app/terms-and-conditions/page.jsx
+++ src/app/terms-and-conditions/page.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import termsCss from "./styles/term-condition.module.css"
 
 const termsAndConditions = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+      <div className={termsCss.terms_first}>
+        <div className="container">
+          <div className="text-center">
+            <h1>Terms & Conditions</h1>
+            <p>Effective May, 01 2026</p>
+          </div>
+        </div>
+      </div>
+      <div className={termsCss.terms_second}>
+        <div className="container">
+          <div className="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde a voluptas eum itaque doloribus blanditiis adipisci alias voluptate veniam velit pariatur quasi, consequuntur tenetur veritatis repudiandae deserunt in nesciunt dolores possimus. Dolore, labore odio, quasi cum sed ducimus ut voluptatibus alias repellendus iure nisi iusto explicabo ullam sit? Incidunt reiciendis minima, maxime esse quod earum reprehenderit illo quam non cupiditate culpa molestias unde odio quae enim ipsam tenetur! Facere odit sint est vero ipsam doloremque. Eos distinctio impedit atque. Dolor fuga molestias sed? Beatae fugiat ex a nisi quod veritatis! Voluptatum voluptate nobis reiciendis, dicta facilis debitis similique vel quis aspernatur mollitia enim omnis temporibus incidunt itaque laboriosam ab rerum repellat dolores nostrum odit quam fuga nulla veniam. Quia unde fugit ab ea iure esse, expedita eligendi odit voluptatibus animi earum magni porro harum voluptatum, architecto obcaecati repudiandae quis nulla, eius nostrum quos nihil at iste praesentium. Quae, dolores. Vel voluptas error reiciendis laboriosam libero repellat, deserunt, molestiae nemo soluta sint illo enim quos. Cum deserunt necessitatibus magnam nihil eligendi nesciunt illo totam accusamus at tenetur natus atque harum, laudantium unde esse magni sunt! Quia excepturi voluptates laudantium dicta ipsa ad vero delectus veritatis? Illum laboriosam magni dignissimos error voluptatum!</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
