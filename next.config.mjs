aca133cb34a71f6b6400fedaa6947a6c08b6d00e
Zahid hasan <zmzahidhasan181@gmail.com>
2026-06-29 17:53:31 +0600

    fix routing and  create structural route

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // output: "export",
+//   exportTrailingSlash: true,
+//   /* config options here */
+//   reactCompiler: true,
+//   devIndicators: true,
+//   images: {
+//     unoptimized: true,
+//   }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  exportTrailingSlash: true,
-  /* config options here */
   reactCompiler: true,
-  devIndicators: true,
+
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
